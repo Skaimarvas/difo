9c8ba88bbd44328d9b95fb8d38a1ca31f1938a87
Kaim Arvas <skaimarvas@gmail.com>
2024-09-14 23:12:01 +0300

    create and update page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,62 @@
-import Image from "next/image";
+import Time from "@/components/Time";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="flex flex-row justify-between h-screen w-full">
+      {/* <div className="bg-gradient-to-b from-purpleTransparent via-purpleOpaque to-purpleTransparent h-2 w-full rotate-90"></div> */}
+      <section className="flex flex-col w-1/2">
+        <h2 className="bg-gradient-to-r from-[#52FF00] to-[#016DD0] bg-clip-text text-transparent text-3xl text-center font-extrabold">
+          Pre-sale planning of the $DIFO Token
+        </h2>
+        <p className="text-center">
+          The highly anticipated discounted public sale rounds of $DIFO token
+          are now live.
+        </p>
+        <hr />
+        <h4 className="bg-gradient-to-r from-[#FFAC00] to-[#AB00FF] bg-clip-text text-transparent  text-center font-extrabold">
+          Round 1 Total Token: 43,950.000 $DIFO
+        </h4>
+        <div className="flex flex-col w-full">
+          <div className="flex justify-end">
+            <span className="bg-gradient-to-r from-[#AB00FF] to-[#FFAC00] bg-clip-text text-transparent text-right font-extrabold">
+              %100
+            </span>
+          </div>
+          <div className="flex justify-center">
+            <span>progress bar</span>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div className="flex justify-evenly relative">
+          <Time timeData="21" timeType="Days" />
+          <Time timeData="12" timeType="Hours" />
+          <Time timeData="06" timeType="Minutes" />
+          <Time timeData="57" timeType="Minutes" />
+          <div className="absolute top-5">
+            <span className="bg-gradient-to-r from-[#FFAC00] to-[#AB00FF] bg-clip-text text-transparent text-5xl text-center font-extrabold">
+              :
+            </span>
+          </div>
+        </div>
+        <hr />
+        <h4 className="text-[#FFAC00] text-2xl text-center font-bold">
+          Total Token Supply (TTS):
+        </h4>
+        <p className="text-center">
+          <span className="font-bold">MAX:</span>It is limited to 879,000,000
+          million $DIFO.
+        </p>
+        <hr />
+        <h2 className="bg-gradient-to-r from-[#52FF00] to-[#016DD0] bg-clip-text text-transparent text-3xl text-center font-extrabold">
+          $DIFO Smart Contract Address:
+        </h2>
+      </section>
+      {/* <div className="bg-gradient-to-b from-purpleTransparent via-purpleOpaque to-purpleTransparent h-2 w-full rotate-90"></div> */}
+      <section className="flex flex-col w-1/2">
+        <h2 className="bg-gradient-to-r from-[#016DD0] to-[#52FF00] bg-clip-text text-transparent text-3xl text-center font-extrabold">
+          Pre-sale Details of the $DIFO Token
+        </h2>
+      </section>
+      {/*    <div className="bg-gradient-to-b from-purpleTransparent via-purpleOpaque to-purpleTransparent h-2 w-full rotate-90"></div> */}
     </div>
   );
 }

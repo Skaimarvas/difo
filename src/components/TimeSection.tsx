f710ad317042a4105b9cc46cf4b9ca43a6bc1e52
Kaim Arvas <skaimarvas@gmail.com>
2024-09-15 23:50:52 +0300

    update text top

--- src/components/TimeSection.tsx
+++ src/components/TimeSection.tsx
@@ -7,8 +7,8 @@ const TimeSection = () => {
       <Time timeData="12" timeType="Hours" />
       <Time timeData="06" timeType="Minutes" />
       <Time timeData="57" timeType="Second" />
-      <div className="absolute top-5 sm:top-7">
-        <span className="bg-gradient-to-r from-[#FFAC00] to-[#AB00FF] bg-clip-text text-transparent text-5xl text-center font-extrabold">
+      <div className="absolute top-8 sm:top-9">
+        <span className="bg-gradient-to-r from-[#FFAC00] to-[#AB00FF] bg-clip-text text-transparent text-5xl sm:text-4xl text-center font-extrabold">
           :
         </span>
       </div>

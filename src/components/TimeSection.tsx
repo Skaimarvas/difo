597916e3cfbd71a854e26e69337e2ec92b8e00f9
Kaim Arvas <skaimarvas@gmail.com>
2024-09-15 11:13:05 +0300

    update time components

---
+++ src/components/TimeSection.tsx
@@ -0,0 +1,19 @@
+import Time from "./molecules/Time";
+
+const TimeSection = () => {
+  return (
+    <div className="flex justify-evenly lg:gap-0 xl:gap-24  relative my-8">
+      <Time timeData="21" timeType="Days" />
+      <Time timeData="12" timeType="Hours" />
+      <Time timeData="06" timeType="Minutes" />
+      <Time timeData="57" timeType="Minutes" />
+      <div className="absolute top-5 sm:top-7">
+        <span className="bg-gradient-to-r from-[#FFAC00] to-[#AB00FF] bg-clip-text text-transparent text-5xl text-center font-extrabold">
+          :
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default TimeSection;
---
+++ src/components/molecules/Time.tsx
@@ -0,0 +1,31 @@
+import React, { FC } from "react";
+
+interface Iprops {
+  timeType: string;
+  timeData: string;
+}
+
+const Time: FC<Iprops> = ({ timeData, timeType }) => {
+  return (
+    <div className="flex flex-col items-center justify-center bg-black">
+      <span className="text-xs sm:text-base font-bold text-transparent bg-clip-text bg-gradient-to-r from-[#FFAC00] to-[#AB00FF]">
+        {timeType}
+      </span>
+      <div className="relative bg-black rounded-xl">
+        <div className="rounded-xl p-1 bg-gradient-to-r from-[#FFAC00] to-[#AB00FF]">
+          <div className="flex items-center justify-center h-14 w-12 sm:h-16 sm:w-14 bg-black rounded-lg">
+            <span className="text-2xl sm:text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500">
+              {timeData}
+            </span>
+          </div>
+        </div>
+      </div>
+      <div className="mt-2">
+        <div className="h-[2px] w-16 bg-gradient-to-r from-[#016DD000] via-[#00D1FF] to-[#016DD000]"></div>
+        <div className="h-[2px] w-16 bg-gradient-to-r from-[#016DD000] via-[#00D1FF] to-[#016DD000] opacity-50"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Time;
